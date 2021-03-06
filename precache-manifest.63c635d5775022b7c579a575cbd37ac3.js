c313a6b7f6c10d5c20f6b70c4076f652729d628a
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2021-03-06 11:30:06 +0100

    Updates

---
+++ precache-manifest.63c635d5775022b7c579a575cbd37ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f046be5fc861785feef10bce2e0a8e7d",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "e515e3edc68f35778908",
+    "url": "/birthday-reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e515e3edc68f35778908",
+    "url": "/birthday-reminder/static/js/main.7eeaa650.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
